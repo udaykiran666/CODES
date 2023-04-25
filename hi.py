521c81c02e739a3e1279a09c3de503b5db1783fe
udaykiran666 <85557923+udaykiran666@users.noreply.github.com>
2023-04-25 19:20:00 +0530

    Update hi.py

--- hi.py
+++ hi.py
@@ -1,1 +1,31 @@
+""
+test custom django management commands
+"""
+from unittest.mock import patch
+from psycopg2 import OperationalError as Psycopg2Error
 
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+
+@patch('core.management.commands.wait_for_db.Command.check')
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db_ready(self,patched_check):
+        """test waiting for datbase if database is ready"""
+        patched_check.return_value=True
+        call_command('wait_for_db')
+        patched_check.assert_called_once_with(database=['default'])
+    
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self,patched_sleep,patched_check):
+        """test waiting for database when getting opeartional error"""
+        patched_check.side_effect=[Psycopg2Error] *2 + [OperationalError]*3+[True]   #so for first 2times we raise psycopg error and next 3times we risr operational error
+        
+        call_command('wait_for_db')
+
+        self.assertEqual(patched_check.call_count,6)
+        # patch_check.assert_called_with(database=['default'])
+        patch_check.assert_called_with(databases=['default'])
+        
